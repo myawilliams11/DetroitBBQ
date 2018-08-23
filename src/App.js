73e770700135d2dfc59b1209e827345183c36e72
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-08-23 13:52:30 -0400

    created 2 more pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Order from './Order'
+import History from './History'
+import Map from './Map'
 
 class App extends Component {
 state = {
@@ -16,12 +18,25 @@ onClick = (pageName) => (e) => {
     return (
       <div className="App">
     {this.state.renderPage === 'Order' ? <Order />: null}
+    {this.state.renderPage === 'History' ? <History />: null}
+    {this.state.renderPage === 'Map' ? <Map />: null}
       <button 
       onClick= {this.onClick('Order')}
       disabled={this.state.renderPage === 'Order' ? true: false}>
       Order Form
       </button>
-      <button onClick= {this.onClick('Home')}>Home</button>
+      <button 
+      onClick= {this.onClick('Home')}>
+      Home
+      </button>
+      <button onClick= {this.onClick('History')}
+      disabled={this.state.renderPage === 'History' ? true: false}>
+      History
+      </button>
+      <button onClick= {this.onClick('Map')}
+      disabled={this.state.renderPage === 'Map' ? true: false}>
+      Map
+      </button>
     </div>
     );
   }
---
+++ src/history.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class History extends React.Component {
+
+render(){
+    return (
+        <p>This is the about us page!</p>
+    )
+    
+}}
+
+export default History;
