e5cf79fdf251cc4223eb3948c106aea5b46ea839
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-08-24 11:14:17 -0400

    added dropdowns and text area on one of the pages

---
+++ src/FoodDrop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Dropdown, Menu } from 'semantic-ui-react'
+
+const options = [
+  { key: 1, text: 'BBQ', value: 1 },
+  { key: 2, text: 'Ribs', value: 2 },
+  { key: 3, text: 'Chicken', value: 3 },
+]
+
+const DropdownExampleSimple = () => (
+  <Menu compact>
+    <Dropdown text='Menu' options={options} simple item />
+  </Menu>
+)
+
+export default DropdownExampleSimple
